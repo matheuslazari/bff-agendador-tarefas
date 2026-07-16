0a947609d1cc35ca155b804842a970e05da49f86
Matheus Lazari <mthslazari@gmail.com>
2026-07-16 16:14:02 -0300

    feat: implementa cron

--- src/main/java/com/matheus/bffagendadortarefas/infraestructure/client/EmailClient.java
+++ src/main/java/com/matheus/bffagendadortarefas/infraestructure/client/EmailClient.java
@@ -3,11 +3,13 @@ package com.matheus.bffagendadortarefas.infraestructure.client;
 
 import com.matheus.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacaop", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 
     }
